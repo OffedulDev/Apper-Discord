ac0cfcdd7b86ad21b69794cc0513f20617c5f2f9
OffedulDev <aloneforever.commercial@gmail.com>
2022-07-18 11:40:06 +0200

    Pull request initiaL COMMIT.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+from click import option
 from Utility import *
 import interactions
 
@@ -19,8 +20,41 @@ import interactions
 # Embed.embed
 
 
+# Temp Var
+Applications = []
 
-# Setup Command
+# Create Command
+_SuccessEmbed = Embed(title="Application Created Correctly ✅", 
+                    description="Your application was create correctly, you can now send an embed to fill it with /sendApplication",
+                    color=colors.green)
+_ReviewEmbed = Embed(title="Application Review 🔋", 
+                    description="Please review the application and the fields that you provided. Make sure that it all fits your needs and then press the Green button, otherwhise just press the red button and try again.",
+                    color=colors.magenta)
 
+async def CreateApplication(ctx, options):
+    appname = options['appname']
+    app_name = appname
+    fields = [options['field0'], options['field1'], options['field2'], options['field3'], options['field4']]
+    async def confirmCreation(ctx):
+        App = Application(guild=ctx.get_guild().id, app_name=app_name, app_fields=fields)
+        Applications.append(App)
+
+        await ctx.send(embeds=_SuccessEmbed.embed)
+        return
+        
+    async def discardCreation(ctx):
+        await ctx.send("Ignoring changes, discarding application.")
+        return
+
+    _ActionRow = ActionRow()
+    _ActionRow.add_button(callback=discardCreation, label="Discard ❌", custom_id="discard" + ctx.get_guild().id, style=interactions.ButtonStyle.DANGER)
+    _ActionRow.add_button(callback=confirmCreation, label="Confirm ✅", custom_id="accept" + ctx.get_guild().id, style=interactions.ButtonStyle.DANGER)
+    await ctx.send(embeds=_ReviewEmbed.embed, components=_ActionRow)
+
+
+CreateCommand = Command(Name="create", Desc="Create a new application.", Admin=True, CallBack=CreateApplication)
+CreateCommand.add_option(Name="appname", Desc="Name of the application.", Required=True)
+for i in range(5): CreateCommand.add_option(Name=f"field{i}", Desc=f"Field of the Application. {i}" , Required=True)
+CreateCommand.build_command()
 
 Client.start()
--- Utility.py
+++ Utility.py
@@ -1,15 +1,44 @@
 # Utility Module
+from code import interact
 import interactions
 from bot_token import Bot_Token
 
-Client = interactions.Client(token=Bot_Token, intents=interactions.Intents.ALL)
+Client = interactions.Client(token=Bot_Token, intents=interactions.Intents.GUILD_MESSAGE_CONTENT | interactions.Intents.GUILDS)
 
+## Discord Utilities
+
+class colors:
+    default = 0
+    teal = 0x1abc9c
+    dark_teal = 0x11806a
+    green = 0x2ecc71
+    dark_green = 0x1f8b4c
+    blue = 0x3498db
+    dark_blue = 0x206694
+    purple = 0x9b59b6
+    dark_purple = 0x71368a
+    magenta = 0xe91e63
+    dark_magenta = 0xad1457
+    gold = 0xf1c40f
+    dark_gold = 0xc27c0e
+    orange = 0xe67e22
+    dark_orange = 0xa84300
+    red = 0xe74c3c
+    dark_red = 0x992d22
+    lighter_grey = 0x95a5a6
+    dark_grey = 0x607d8b
+    light_grey = 0x979c9f
+    darker_grey = 0x546e7a
+    blurple = 0x7289da
+    greyple = 0x99aab5
 async def get_channel(id: int):
     try:
         return interactions.Channel(**await Client._http.get_channel(id), _client=Client._http)
     except:
         print("Error getting channel")
         return None
+
+## Discord (sub)Classes
 
 class Embed:
     def __init__(self, title="None", description="None", color=0x336EFF):
@@ -66,7 +95,7 @@ class ActionRow:
 
     def build_row(self):
         return interactions.ActionRow(components=self.actions)
-
+    
 class Command:
     def __init__(self, CallBack, Name="", Desc="", Admin=False):
         self.name = Name
@@ -75,7 +104,7 @@ class Command:
         self.callback = CallBack
         self.admin = Admin
 
-    def add_option(self, Name, Desc, Required, Option_Type):
+    def add_option(self, Name, Desc, Required, Option_Type=interactions.OptionType.STRING):
         self.options.append(interactions.Option(
                 name=Name,
                 description=Desc,
@@ -92,5 +121,46 @@ class Command:
             options=self.options,
             default_member_permissions=interactions.Permissions.ADMINISTRATOR if self.admin else interactions.Permissions.SPEAK
         )
-        async def execute(ctx, **options):
-            await callback(ctx, options)
+        async def execute(ctx, **kargs):
+            await callback(ctx, kargs)
+
+## Bot Classes 
+
+class Application:
+    def __init__(self, guild, app_name, app_fields):
+        self.app_name = app_name
+        self.app_fields = app_fields
+        self.guild = guild
+        
+        async def ModalCallback(ctx, answers):
+            for answer in answers:
+                await ctx.send(answer)
+
+        self.AppModal = Modal(title=self.app_name, custom_id=self.app_name.lower(), callback=ModalCallback)
+        for field in app_fields: self.AppModal.add_textinput(label=field, custom_id=field.lower())
+            
+    async def sendActionRow(self, ctx):
+        StartEmbed = Embed(title="Starting Application ✅", 
+                           description="You should be prompted with your application in a moment. Thanks for choosing Apper!", 
+                           color=colors.green)
+        
+        ActionRowEmbed = Embed(title=self.app_name + " Application", 
+                               description="Press the button below to start this application, under here you can find every question asked in the application.", 
+                               color=colors.teal)
+
+        for Field in self.app_fields: ActionRowEmbed.add_field(name=self.app_field.index(Field), value=Field, inline=True)
+
+        async def startApplication(ctx):
+            await ctx.send(embeds=StartEmbed.embed)
+            await ctx.popup(self.AppModal)
+
+        _ActionRow = ActionRow()
+        _ActionRow.add_button(callback=startApplication, 
+                              label="Start Application (" + self.app_name + ")", 
+                              custom_id=self.app_name.lower())
+
+        await ctx.send(embeds=ActionRowEmbed.embed, components=_ActionRow)
+        
+
+
+                    
